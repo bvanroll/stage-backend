cf13c06ba87096f87fa1657204c7e885524999a9
beppe <beppe.vanrolleghem@gmail.com>
2020-03-16 12:14:50 +0100

    submodule en src

---
+++ src/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import jsonify
+app = Flask(__name__)
+
+
+@app.route('/')
+def doRequest():
+    data = {
+        "serverName": "serverB",
+        "version": "master",
+        "success": "true"
+    }
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=6000)
